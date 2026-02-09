a930c0751980e6c3c8f374543529ffe133f2bb8c
z274202081 <z274202081@gmail.com>
2026-02-09 22:17:07 +0800

    Change login and subscription passwords
    Updated WEB_PASSWORD and SUB_PASSWORD for new credentials.

--- _worker.js
+++ _worker.js
@@ -6,8 +6,8 @@ import { connect } from 'cloudflare:sockets';
 
 // --- 基础账号与网络配置 ---
 let UUID = "06b65903-406d-4a41-8463-6fd5c0ee7798"; //修改可用的uuid
-const WEB_PASSWORD = "123456";  //修改你的登录密码
-const SUB_PASSWORD = "123456";  //修改你的订阅密码
+const WEB_PASSWORD = "s274202081";  //修改你的登录密码
+const SUB_PASSWORD = "z274202081";  //修改你的订阅密码
 const DEFAULT_PROXY_IP = atob("UHJveHlJUC5VUy5DTUxpdXNzc3MubmV0"); // 支持多ProxyIP，使用逗号分隔
 const DEFAULT_SUB_DOMAIN = atob("c3ViLmNtbGl1c3Nzcy5uZXQ=");      // 支持多订阅域名，使用逗号分隔
 const DEFAULT_CONVERTER = atob("aHR0cHM6Ly9zdWJhcGkuY21saXVzc3NzLm5ldA=="); // 支持多转换器，使用逗号分隔
